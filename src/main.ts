1263084dbac35560ecf9ab2f4c30a134d7ee5707
yijie4188 <yijie4188@gmail.com>
2021-11-19 14:32:50 +0800

    feat: import element plus and set theme primary color

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+
+import './assets/styles/css-var.scss'
+import './assets/styles/element.scss'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(ElementPlus)
+  .mount('#app')
