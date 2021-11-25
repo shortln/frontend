9c02c374ea28bc47f3f5da5af4c27e14a6ff446d
yijie4188 <yijie4188@gmail.com>
2021-11-26 00:13:22 +0800

    upd: axios interceptors

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,14 +1,36 @@
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosInstance, AxiosResponse, AxiosError, AxiosRequestConfig } from 'axios'
+
+interface ErrorResponseData {
+  message: string
+}
 
 export interface Api {
   moduleName: string
 }
 
-export default class AbsApis implements Api {
-  moduleName = ''
-  readonly $h: AxiosInstance
+export default class AbsApis {
+  _h?: AxiosInstance = undefined
 
-  constructor() {
-    this.$h = axios.create({ baseURL: '/api/' + this.moduleName })
+  get $h(): AxiosInstance {
+    if (!this._h) {
+      // @ts-ignore
+      this._h = axios.create({ baseURL: '/api/' + this.moduleName })
+      this._h.interceptors.request.use(
+        (config: AxiosRequestConfig) => config,
+        (error: any) => Promise.reject(error)
+      )
+      this._h.interceptors.response.use((response: AxiosResponse) => {
+        if (response.status === 202) {
+          return new Promise(resolve => setTimeout(
+            () => resolve(this.$h.request(response.config)), response.config?.params.nextTimeout || 3000
+          ))
+        }
+        return response.data
+      }, (error: AxiosError<ErrorResponseData>) => {
+        return Promise.reject(error)
+      })
+    }
+
+    return this._h
   }
 }
