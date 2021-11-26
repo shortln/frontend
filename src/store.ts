6a414eeb0fa0025d571c67c9c601ba8dc5b9811b
yijie4188 <yijie4188@gmail.com>
2021-11-26 14:07:16 +0800

    upd: logout logic

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { createStore } from 'vuex'
-import { AccountOut } from './apis/accountApis'
+import accountApis, { AccountOut } from './apis/accountApis'
 
 export type Account = AccountOut
 
@@ -9,9 +9,21 @@ export default createStore({
       id: -1
     } as Account
   },
+  getters: {
+    isLogin: state => state.account.id !== -1
+  },
   mutations: {
     setCurU: (state, account: Account) => {
       state.account = account
+    }
+  },
+  actions: {
+    logout: async ({ getters, state }) => {
+      if (!getters.isLogin || !state.account.username) {
+        throw new Error('用户未登陆。')
+      }
+      await accountApis.logout(state.account.username)
+      state.account = { id: -1 }
     }
   }
 })
