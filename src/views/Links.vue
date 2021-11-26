593ced9970db5887bb79b029dc4e158a796cc456
yijie4188 <yijie4188@gmail.com>
2021-11-26 22:56:01 +0800

    feat: links api and links panel animation

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -1,52 +1,59 @@
 <template>
   <div class="center">
-    <div class="left-panel">
+    <div v-loading="isLoading" class="left-panel">
       <div class="opts">
         <el-tooltip content="添加新分组">
           <el-button :icon="Plus"/>
         </el-tooltip>
+        <el-tooltip content="刷新">
+          <el-button :icon="Refresh" @click="refresh"/>
+        </el-tooltip>
       </div>
-      <template v-for="linksGroup in linksGroups" :key="linksGroup.id">
+      <transition-group name="empty-to-full">
+        <el-empty
+          v-if="linksGroups.length === 0"
+          key="empty"
+          class="empty-to-full-item"
+          description="还没有短链接分组哦~创建一个吧"/>
         <div
-          class="title"
-          :class="{
-            active: activeGid !== -1 && activeGid === linksGroup.id
-          }">
-          <el-icon
-            class="prepend"
-            @click="activeGid = linksGroup.id">
-            <arrow-right/>
-          </el-icon>
-          <span class="name">{{ linksGroup.name }}</span>
-          <span class="opts">
-            <el-icon class="opt"><plus/></el-icon>
-          </span>
-        </div>
-        <el-scrollbar
-          :ref="`scrollbar${linksGroup.id}`"
-          class="children"
-          :class="{
-            active: activeGid !== -1 && activeGid === linksGroup.id
-          }"
-          height="210px">
-          <div
-            v-for="link in linksGroup.links" :key="link.id"
-            class="child"
-            :class="{
-              active: activeId === link.id
-            }">
-            <span class="name" @click="
-              activeId = link.id;
-              $router.push(`/links/${link.id}/detail`)
-            ">
-              <el-icon class="prepend"><ln/></el-icon>{{ link.title }}
-            </span>
-            <span class="opts">
-              <el-icon class="opt"><setting/></el-icon>
-            </span>
+          v-for="lg in linksGroups" :key="lg.id"
+          class="links-group empty-to-full-item"
+          :class="{ active: isActive(lg) }">
+          <div class="title">
+            <el-icon
+              class="prepend"
+              @click="activeGid = lg.id">
+              <arrow-right/>
+            </el-icon>
+            <span class="name">{{ lg.name }}</span>
+            <span class="opts"><el-icon class="opt"><plus/></el-icon></span>
           </div>
-        </el-scrollbar>
-      </template>
+          <el-scrollbar
+            :ref="`scrollbar${lg.id}`"
+            class="children"
+            :style="{
+              height: `${ (isActive(lg) ? lg.links.length : 0) * 42 }px`
+            }"
+            :height="`${ lg.links.length * 42 }px`">
+            <div
+              v-for="link in lg.links" :key="link.id"
+              class="child"
+              :class="{
+                active: activeId === link.id
+              }">
+              <span class="name" @click="
+                activeId = link.id;
+                $router.push(`/links/${link.id}/detail`)
+              ">
+                <el-icon class="prepend"><ln/></el-icon>{{ link.title }}
+              </span>
+              <span class="opts">
+                <el-icon class="opt"><setting/></el-icon>
+              </span>
+            </div>
+          </el-scrollbar>
+        </div>
+      </transition-group>
     </div>
     <div class="container">
       <router-view/>
@@ -55,28 +62,27 @@
 </template>
 
 <script lang="ts" setup>
-import { ArrowRight, Link as Ln, Plus, Setting } from '@element-plus/icons'
+import { ArrowRight, Link as Ln, Plus, Refresh, Setting } from '@element-plus/icons'
 import { getCurrentInstance, nextTick, onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { ElScrollbar } from 'element-plus'
+import linkApis, { LinksGroupOut } from '../apis/linkApis'
+import useLoading from '../hooks/useLoading'
 
-interface Link {
-  id: number
-  title: string
-}
-
-interface LinksGroup {
-  id: number
-  name: string
-  links: Link[]
-}
+type LinksGroup = LinksGroupOut
 
 const
   ctx = getCurrentInstance(),
   route = useRoute(),
   linksGroups = ref<LinksGroup[]>([]),
   activeId = ref(-1),
-  activeGid = ref(-1)
+  activeGid = ref(-1),
+  isActive = (lg: LinksGroup) => activeGid.value !== -1 && activeGid.value === lg.id,
+  { isLoading, nAsyncFun: refresh } = useLoading(async () => {
+    linksGroups.value = []
+    linksGroups.value = await linkApis.getLinksGroups({ str: '' })
+      .then(p => p.items)
+  })
 
 watch(linksGroups, lgs => {
   if (route.name === 'link-detail') {
@@ -96,20 +102,8 @@ watch(linksGroups, lgs => {
   }
 })
 
-onMounted(() => {
-  linksGroups.value = [{
-    id: 1,
-    name: '默认分组',
-    links: [...new Array(10).keys()].map(id => ({
-      id, title: `随便的第 ${id} 个短链接`
-    }))
-  }, {
-    id: 2,
-    name: '频道',
-    links: [...new Array(4).keys()].map(id => id + 10).map(id => ({
-      id, title: `QQ 频道 ${id}`
-    }))
-  }]
+onMounted(async () => {
+  await refresh()
 })
 </script>
 
@@ -118,6 +112,44 @@ onMounted(() => {
   color: var(--color-primary);
   border: 1px solid var(--color-primary);
 }
+.empty-to-full {
+  &-item {
+    transition: all 0.3s ease;
+    &.el-empty {
+      position: absolute;
+      top: 50px;
+      width: 100%;
+    }
+  }
+  &-enter {
+    &-to {
+      opacity: 1;
+      &.links-group {
+        transform: translateX(0);
+      }
+    }
+    &-active {
+      opacity: 0;
+      &.links-group {
+        transform: translateX(100%);
+      }
+    }
+  }
+  &-leave {
+    &-to {
+      opacity: 0;
+      &.links-group {
+        transform: translateX(100%);
+      }
+    }
+    &-active {
+      opacity: 1;
+      &.links-group {
+        transform: translateX(0);
+      }
+    }
+  }
+}
 div.center {
   $mar-between: 100px;
 
@@ -125,81 +157,85 @@ div.center {
   margin: 20px #{$mar-between};
   width: calc(100% - #{$mar-between * 2});
   > div.left-panel {
-    padding: 0 20px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    row-gap: 5px;
+    margin: 0 20px;
     width: 300px;
+    overflow: hidden;
     > div.opts {
       display: flex;
       align-items: center;
       padding: 5px;
     }
-    > div.title {
-      display: flex;
-      padding: 5px;
-      justify-content: space-between;
-      align-items: center;
-      font-size: 18px;
-      border: 1px solid #0000;
-      border-radius: 4px;
-      box-sizing: border-box;
-      transition: 0.3s;
-      user-select: none;
-      &:not(:first-child) {
-        margin-top: 10px;
+    > div.links-group {
+      > div.title {
+        display: flex;
+        padding: 5px;
+        justify-content: space-between;
+        align-items: center;
+        font-size: 18px;
+        border: 1px solid #0000;
+        border-radius: 4px;
+        box-sizing: border-box;
+        transition: 0.3s;
+        user-select: none;
+        &:not(:first-child) {
+          margin-top: 10px;
+        }
+        &:hover {
+          border: 1px solid var(--color-primary);
+          box-shadow: 0 0 16px var(--color-shandow);
+        }
+        > i.el-icon.prepend { transition: 0.3s; }
+        > span.opts {
+          display: flex;
+          align-items: center;
+        }
+        > i.el-icon.prepend, > span.opts > i.el-icon.opt {
+          cursor: pointer;
+        }
       }
-      &:hover {
-        border: 1px solid var(--color-primary);
-        box-shadow: 0 0 16px var(--color-shandow);
-      }
-      &.active {
+      &.active > div.title {
         border: 1px solid var(--color-primary);
         > i.prepend.el-icon {
           transform: rotate(90deg);
         }
       }
-      > i.el-icon.prepend { transition: 0.3s; }
-      > span.opts {
-        display: flex;
-        align-items: center;
-      }
-      > i.el-icon.prepend, > span.opts > i.el-icon.opt {
-        cursor: pointer;
-      }
-    }
-    > div.children {
-      max-height: 0;
-      transition: 0.3s;
-      &.active {
-        max-height: 210px;
-      }
-      div.child {
-        display: flex;
-        padding: 5px;
-        margin: 10px 10px 0 40px;
-        justify-content: space-between;
-        align-items: center;
-        font-size: 15px;
-        border: 1px solid #0000;
-        border-radius: 4px;
+      > div.children {
         transition: 0.3s;
-        &:hover {
-          @include __;
+        div.child {
+          display: flex;
+          padding: 5px;
+          margin: 10px 10px 0 40px;
+          height: 20px;
+          justify-content: space-between;
+          align-items: center;
+          font-size: 15px;
+          border: 1px solid #0000;
+          border-radius: 4px;
+          transition: 0.3s;
+          &:hover {
+            @include __;
 
-          box-shadow: 0 0 16px var(--color-shandow);
-        }
-        &.active {
-          @include __;
-        }
-        > span.name {
-          display: flex;
-          align-items: center;
-          > i.el-icon.prepend { margin-right: 5px; }
-        }
-        > span.name, > span.opts > i.el-icon.opt {
-          cursor: pointer;
-        }
-        > span.opts {
-          display: flex;
-          align-items: center;
+            box-shadow: 0 0 16px var(--color-shandow);
+          }
+          &.active {
+            @include __;
+          }
+          > span.name {
+            display: flex;
+            align-items: center;
+            > i.el-icon.prepend { margin-right: 5px; }
+          }
+          > span.name, > span.opts > i.el-icon.opt {
+            cursor: pointer;
+          }
+          > span.opts {
+            display: flex;
+            align-items: center;
+          }
         }
       }
     }
