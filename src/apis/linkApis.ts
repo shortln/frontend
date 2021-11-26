815a0b003c4321fee91978098ee0d2778ac31904
yijie4188 <yijie4188@gmail.com>
2021-11-26 22:55:10 +0800

    feat: link apis

---
+++ src/apis/linkApis.ts
@@ -0,0 +1,21 @@
+import AbsApis, { Api, Pagination, Query } from './index'
+import * as queryString from 'querystring'
+
+export interface LinkOut {
+  id: number
+  title: string
+}
+
+export interface LinksGroupOut {
+  id: number
+  name: string
+  links: LinkOut[]
+}
+
+export default new class LinkApis extends AbsApis implements Api {
+  moduleName = 'links'
+
+  createLinksGroup = (name: string) => this.$h.post<any, LinksGroupOut>('/groups', { name })
+
+  getLinksGroups = (q: Query) => this.$h.get<any, Pagination<LinksGroupOut>>(`/groups?${queryString.stringify(q)}`)
+}
