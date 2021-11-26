202745bf2334047189c2ba5c4847e7db5c38c563
yijie4188 <yijie4188@gmail.com>
2021-11-26 12:57:46 +0800

    upd: set return type

--- src/apis/accountApis.ts
+++ src/apis/accountApis.ts
@@ -1,10 +1,14 @@
 import AbsApis, { Api } from './index'
 
+export interface AccountOut {
+  id: number
+  username?: string
+  nickname?: string
+}
+
 export default new class AccountApis extends AbsApis implements Api {
   moduleName = 'accounts'
 
-  login = (username: string, password: string) => {
-    return this.$h.post(
-      `/${username}/login`, { password })
-  }
+  login = (username: string, password: string) => this.$h.post<any, AccountOut>(
+    `/${username}/login`, { password })
 }
